52d14aad3e4453e99f28db77f8aebf5e5cfab020
foolsgold5 <lockeds@yahoo.com>
2021-04-12 06:30:38 -0400

    scatterplot work

--- src/views/Explore/views/ExploreContent.vue
+++ src/views/Explore/views/ExploreContent.vue
@@ -1,34 +1,76 @@
 <template>
-    <div>
-        This is Scatter Plot for Content
-        <!--button @click.prevent="$emit('onHoverPresident')">Click me </button-->
+  <div>
+    This is Scatter Plot for Content
+    <!--button @click.prevent="$emit('onHoverPresident')">Click me </button-->
 
-        <div v-for="(item, index) of presidentsData " :key="index">
-            <div @click="$emit('onPresidentSelected', item)" @mouseenter="$emit('onPresidentHover', item)" :class="selectedPresidentDetails.Id === item.Id && $style.activePresident">
-                {{item.Name}} - {{item.Year}}
-            </div>
-            <!--this is a div temporary, it could have been button, scatterplot goes here-->
-        </div>
+    <!--<div v-for="(item, index) of presidentsData" :key="index">
+      <div
+        @click="$emit('onPresidentSelected', item)"
+        @mouseenter="$emit('onPresidentHover', item)"
+        :class="
+          selectedPresidentDetails.Id === item.Id && $style.activePresident
+        "
+      >
+        {{ item.Name }} - {{ item.Year }}
+      </div>
+
+    </div>-->
+    <!--this is a div temporary, it could have been button, scatterplot goes here-->
+    <div>
+      <Scatterplot
+        :points="presidentsData"
+        xVar="x"
+        yVar="y"
+        keyVar="Party"
+        :getFill="colorScale"
+        @mouseenter="onMouseEnter"
+        @click="$emit('onPresidentSelected', $event)"
+      />
+      <!--h4 v-if="speciesFilter">Species: {{ speciesFilter }}</h4-->
+      <!--:onClick="value => setQueryParam('species', value)"-->
     </div>
+  </div>
 </template>
 <script>
+import { scaleOrdinal, schemeCategory10 } from 'd3'
+import Scatterplot from '../components/Scatterplot'
 export default {
-    props: {
-        presidentsData: {
-            type: Array,
-            required: true
-        },
-        selectedPresidentDetails: {
-            type: Object,
-            default: () => ({})
-        }
+  components: {
+    Scatterplot,
+  },
+  props: {
+    presidentsData: {
+      type: Array,
+      required: true,
     },
+    selectedPresidentDetails: {
+      type: Object,
+      default: () => ({}),
+    },
+    colorByProperty: {
+      type: String,
+      default: 'Party',
+    },
+  },
+  computed: {
+    colorScale() {
+      return scaleOrdinal(schemeCategory10).domain(this.colorDomain)
+    },
+    colorDomain() {
+      return Array.from(new Set(this.presidentsData.map(d => d[this.colorByProperty])))
+    },
+  },
+  methods: {
+    onMouseEnter(e, point) {
+      console.log('on mouse enter', e, point)
+      this.$emit('onPresidentHover', point)
+    },
+  },
 }
 </script>
 <style module>
 .activePresident {
-    font-weight: bold;
-    color: indigo;
+  font-weight: bold;
+  color: indigo;
 }
 </style>
-
